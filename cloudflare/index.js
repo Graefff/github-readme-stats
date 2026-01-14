7ff30d5e041ae75c259a683f3329182f82e7f939
Gustavo Souza <111467248+Graefff@users.noreply.github.com>
2026-01-14 12:45:03 -0300

    feat: remove pin, wakatime, and gist routes from Express

--- cloudflare/index.js
+++ cloudflare/index.js
@@ -1,11 +1,6 @@
 import { RequestAdapter, ResponseAdapter } from "./adapter.js";
-import { handler as gistHandler } from "../api/gist.js";
 import { handler as indexHandler } from "../api/index.js";
-import { handler as pinHandler } from "../api/pin.js";
 import { handler as topLangsHandler } from "../api/top-langs.js";
-import wakatimeHandler from "../api/wakatime.js";
-import { handler as statusPatInfoHandler } from "../api/status/pat-info.js";
-import { handler as statusUpHandler } from "../api/status/up.js";
 
 export default {
   async fetch(request, env) {
@@ -53,27 +48,13 @@ export default {
 
     if (pathname === "/api") {
       await indexHandler(req, res, env);
-    } else if (pathname === "/api/gist") {
-      await gistHandler(req, res, env);
-    } else if (pathname === "/api/pin") {
-      await pinHandler(req, res, env);
     } else if (pathname === "/api/top-langs") {
       await topLangsHandler(req, res, env);
-    } else if (pathname === "/api/wakatime") {
-      await wakatimeHandler(req, res, env);
-    } else if (pathname === "/api/status/pat-info") {
-      await statusPatInfoHandler(req, res, env);
-    } else if (pathname === "/api/status/up") {
-      await statusUpHandler(req, res, env);
     } else {
       return new Response("not found", { status: 404 });
     }
-    if (pathname === "/api/status/up") {
-      res.setHeader("Cache-Control", "max-age=0"); // no cache
-    } else {
-      res.setHeader("Cache-Control", "max-age=600"); // 10 min
-    }
 
+    res.setHeader("Cache-Control", "max-age=600"); // 10 min
     res.setHeader("X-Robots-Tag", "noindex, nofollow");
     return res.toResponse();
   },
--- express.js
+++ express.js
@@ -1,19 +1,13 @@
 import "dotenv/config";
 import statsCard from "./api/index.js";
-import repoCard from "./api/pin.js";
 import langCard from "./api/top-langs.js";
-import wakatimeCard from "./api/wakatime.js";
-import gistCard from "./api/gist.js";
 import express from "express";
 
 const app = express();
 const router = express.Router();
 
 router.get("/", statsCard);
-router.get("/pin", repoCard);
 router.get("/top-langs", langCard);
-router.get("/wakatime", wakatimeCard);
-router.get("/gist", gistCard);
 
 app.use("/api", router);
 
